35eb3088806926618f9dbdbcdf11ea3a897083ff
Thomas Scheibelhofer <thomas.scheibelhofer@live.at>
2021-02-19 12:51:27 +0100

    Verified that the logout works on Linux Modified readme

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ It's required to include a .env file containing the following data:
 
 - DISCORD_TOKEN={Discord Bot Token}
 - DISCORD_GUILD={Discord Server Name (Guild)}
-- CALENDER_URL={URL to the .ics file}
+- CALENDAR_URL={URL to the .ics file}
--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 # https://realpython.com/how-to-make-a-discord-bot-python/
 import datetime
 import os
+import platform
 
 import aiohttp
+import asyncio
 import discord
 import pytz
 from dotenv import load_dotenv
@@ -12,18 +14,22 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 CALENDAR_URL = os.getenv('CALENDAR_URL')
+MINUTES = int(os.getenv('MINUTES'))
 
 client = discord.Client()
 
+if platform.system() == 'Windows':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-async def get_calendar():
+
+async def get_calendar(url):
     async with aiohttp.ClientSession() as session:
-        async with session.get(CALENDAR_URL) as response:
+        async with session.get(url=url) as response:
             return await response.text()
 
 
 async def get_todays_events():
-    cal_response = await get_calendar()
+    cal_response = await get_calendar(CALENDAR_URL)
     parsed_calendar = Calendar.from_ical(cal_response)
     today = datetime.date.today()
     all_events = (entry for entry in parsed_calendar.walk() if entry.name == "VEVENT")
@@ -42,13 +48,11 @@ async def on_ready():
         for channel in channels:
             remaining_minutes = (event.get("DTSTART").dt - now).seconds / 60
             if channel.name.replace("-", " ") in event.get(
-                    "summary").lower() and 90 > remaining_minutes > 0:
+                    "summary").lower() and MINUTES > remaining_minutes > 0:
                 await channel.send(
                     f"{event.get('summary')}\n\n{event.get('DTSTART').dt.astimezone(pytz.timezone('Europe/Vienna'))}\n\n{event.get('description')}")
+    print("Finished")
     await client.logout()
 
-
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(client.start(TOKEN))
 
 client.run(TOKEN)
